372fa1831481140ef5ebbbb532f696dfb5e783cc
Vegardhgr <vegard.groder@gmail.com>
2022-03-11 15:24:15 +0100

    DeckOfCards: Included the method dealHand

--- src/main/java/no/ntnu/idatg2001/oblig3/cardGame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatg2001/oblig3/cardGame/DeckOfCards.java
@@ -1,11 +1,13 @@
 package no.ntnu.idatg2001.oblig3.cardGame;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * This class represents a deck of cards. 13 cards of each
  * sort (spades, hearts, diamonds and clubs)
+ *
+ * @author Vegard Grøder
+ * @version 1.0.0
  */
 public class DeckOfCards {
     // The class' fields
@@ -25,5 +27,24 @@ public class DeckOfCards {
             }
             cardNumber = 1;
         }
+    }
+
+    /**
+     * Creates and returns a hand of cards
+     * @param numberOfCardsToDraw, amount of playing cards in a hand
+     * @return hand, a list of playing cards
+     */
+    public Collection<PlayingCard> dealHand(int numberOfCardsToDraw) {
+        List<PlayingCard> hand = new ArrayList<>();
+        int card = 0;
+        while (card < numberOfCardsToDraw) {
+            Random rand = new Random();
+            int upperBound = cardList.size();
+            int randomCardFromList = rand.nextInt(upperBound);
+            hand.add(cardList.get(randomCardFromList));
+            cardList.remove(randomCardFromList);
+            card++;
+        }
+        return hand;
     }
 }
