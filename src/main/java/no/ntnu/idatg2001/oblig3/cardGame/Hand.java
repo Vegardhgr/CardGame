81adc1eb099f896aef30e6156e154889a9eca527
Vegardhgr <vegard.groder@gmail.com>
2022-03-13 15:53:50 +0100

    Hand: Added two methods, sumOfFaceOnHand and makeANewDeck

--- src/main/java/no/ntnu/idatg2001/oblig3/cardGame/Hand.java
+++ src/main/java/no/ntnu/idatg2001/oblig3/cardGame/Hand.java
@@ -20,41 +20,58 @@ public class Hand {
         this.cards = new DeckOfCards();
     }
 
-    public List<PlayingCard> makeHand() {
+    private List<PlayingCard> makeHand() throws IllegalArgumentException{
         return new ArrayList<>(cards.dealHand(NUMBER_OF_CARDS_DRAWN));
     }
 
     /**
      * This method checks if a hand is flush or not
+     *
      * @return String, if the hand is flush it returns Flush. If not,
-     * it returns null
+     * it returns an empty string
      */
-    private String checkHand() {
-        List<PlayingCard> hand = makeHand();
-        try {
-            PlayingCard[] sortedHand = new PlayingCard[NUMBER_OF_CARDS_DRAWN];
-            hand.sort(PlayingCard::compareTo);
-
-            int i = 0;
-            for (PlayingCard card : hand) {
-                sortedHand[i] = card;
-                i++;
-            }
-
-            if (checkerForFlush(sortedHand)) {
-                return "Flush";
-            }
-        } catch (NegativeArraySizeException e) {
-            System.out.println(e.getMessage());
+    public String checkHand() throws IllegalArgumentException {
+        List<PlayingCard> hand = new ArrayList<>();
+        hand = makeHand();
+        PlayingCard[] sortedHand = new PlayingCard[NUMBER_OF_CARDS_DRAWN];
+        hand.sort(PlayingCard::compareTo);
+        int i = 0;
+        for (PlayingCard card : hand) {
+            sortedHand[i] = card;
+            i++;
         }
-        return null;
+        if (checkerForFlush(sortedHand)) {
+            return "Flush";
+        }
+
+        return "";
     }
 
-
-    private boolean checkerForFlush (PlayingCard[] sortedHand) {
-        if (sortedHand[0].getSuit() == sortedHand[NUMBER_OF_CARDS_DRAWN-1].getSuit()) {
+    /**
+     * Checks if the cards that you have on hand is a flush
+     * @param sortedHand, an array of the cards on hand in sorted
+     *                    order
+     * @return boolean, true if it is a flush
+     */
+    private boolean checkerForFlush(PlayingCard[] sortedHand) {
+        if (sortedHand[0].getSuit() == sortedHand[NUMBER_OF_CARDS_DRAWN - 1].getSuit()) {
             return true;
         }
         return false;
+    }
+
+    /**
+     * Returns the sum of all the cards on hand
+     * @return int, sum of all the cards
+     */
+    public int sumOfFaceOnHand() {
+        return cards.getCardListFace().sum();
+    }
+
+    /**
+     * This method creates a new deck
+     */
+    public void makeANewDeck() {
+        cards.createANewDeck();
     }
 }
