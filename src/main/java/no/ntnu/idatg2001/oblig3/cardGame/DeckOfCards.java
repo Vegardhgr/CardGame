9d82d69264653ad697fe51db32b6783f8d6ebda5
Vegardhgr <vegard.groder@gmail.com>
2022-03-13 15:47:16 +0100

    DeckOfCards: Added two methods, createNewDeck and getCardListFace

--- src/main/java/no/ntnu/idatg2001/oblig3/cardGame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatg2001/oblig3/cardGame/DeckOfCards.java
@@ -1,6 +1,8 @@
 package no.ntnu.idatg2001.oblig3.cardGame;
 
 import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * This class represents a deck of cards. 13 cards of each
@@ -14,11 +16,49 @@ public class DeckOfCards {
     private final char[] suit = {'S', 'H', 'D', 'C'};
     private int cardNumber = 1;
     private List<PlayingCard> cardList;
+    private List<PlayingCard> hand;
 
     /**
-     * Making the whole deck
+     * Calls createANewDeck method, which creates a new deck,
+     * and initializes the global field, hand
      */
     public DeckOfCards() {
+        this.hand = new ArrayList<>();
+        createANewDeck();
+    }
+
+    /**
+     * Creates and returns a hand of cards
+     * @param numberOfCardsToDraw, amount of playing cards in a hand
+     * @throws IllegalArgumentException, if the number of cards to draw is greater
+     * than the number of cards left in the deck, or if the number of cards to draw is less
+     * than one
+     * @return hand, a list of playing cards
+     */
+    public Collection<PlayingCard> dealHand(int numberOfCardsToDraw) throws IllegalArgumentException {
+        if(numberOfCardsToDraw > cardList.size())
+            throw new IllegalArgumentException("Can not draw that number of cards");
+        if(numberOfCardsToDraw < 1)
+            throw new IllegalArgumentException("Can not draw 0 or less cards");
+
+        new DeckOfCards();
+        this.hand = new ArrayList<>();
+        int card = 0;
+        while (card < numberOfCardsToDraw) {
+            Random rand = new Random();
+            int upperBound = cardList.size();
+            int randomCardFromList = rand.nextInt(upperBound);
+            hand.add(cardList.get(randomCardFromList));
+            cardList.remove(randomCardFromList);
+            card++;
+        }
+        return hand;
+    }
+
+    /**
+     * This method creates a new deck
+     */
+    public void createANewDeck() {
         this.cardList = new ArrayList<>();
         for (int i = 0; i <= 3; i++) {
             while (cardNumber < 14) {
@@ -30,21 +70,10 @@ public class DeckOfCards {
     }
 
     /**
-     * Creates and returns a hand of cards
-     * @param numberOfCardsToDraw, amount of playing cards in a hand
-     * @return hand, a list of playing cards
+     * This method returns a stream of the numbers on the cards
+     * @return
      */
-    public Collection<PlayingCard> dealHand(int numberOfCardsToDraw) {
-        List<PlayingCard> hand = new ArrayList<>();
-        int card = 0;
-        while (card < numberOfCardsToDraw) {
-            Random rand = new Random();
-            int upperBound = cardList.size();
-            int randomCardFromList = rand.nextInt(upperBound);
-            hand.add(cardList.get(randomCardFromList));
-            cardList.remove(randomCardFromList);
-            card++;
-        }
-        return hand;
+    public IntStream getCardListFace() {
+        return this.hand.stream().mapToInt(p -> p.getFace());
     }
 }
