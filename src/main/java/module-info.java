d7c5ec55d1d6f51485e74ce05655d0074817e163
Vegardhgr <vegard.groder@gmail.com>
2022-03-28 15:00:10 +0200

    module-info: Small change. Included opens.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module cardGame {
     requires javafx.controls;
+    opens no.ntnu.idatg2001.oblig3.cardGame;
     exports no.ntnu.idatg2001.oblig3.cardGame;
     exports no.ntnu.idatg2001.oblig3.cardGame.gui;
 }
